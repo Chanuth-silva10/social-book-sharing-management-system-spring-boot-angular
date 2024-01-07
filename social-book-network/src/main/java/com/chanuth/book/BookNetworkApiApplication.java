a23bc8ea0717b3e1e712cef009d221816fe9a666
Chanuth10 <chanuthmaduka1986@gmail.com>
2024-01-07 12:00:00 +0530

    change main package name

--- social-book-network/src/main/java/com/chanuth/book/BookNetworkApiApplication.java
+++ social-book-network/src/main/java/com/chanuth/book/BookNetworkApiApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 @SpringBootApplication
-public class SocialBookNetworkApplication {
+public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialBookNetworkApplication.class, args);
+		SpringApplication.run(BookNetworkApiApplication.class, args);
 	}
 
 }
